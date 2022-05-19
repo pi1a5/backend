16d4ef3d42b49057018b0e66b9a13d897e384dc3
andrezarzur <andrezarzur@hotmail.com>
2022-05-18 23:53:51 -0300

    ajuste

--- models/Ticket.js
+++ models/Ticket.js
@@ -13,8 +13,8 @@ class Ticket {
 
   async checkIfHasTicket(sub){
     try{
-      var id = await knex.select(['id']).table('usuario').where({ sub: sub });
-      var result = await knex.select(['feedback']).table('ticket').where({ id_usuario_aluno: parseInt(id) });
+      var id = await knex.select(['id']).table('usuario').where({ sub: sub }).first();
+      var result = await knex.select(['feedback']).table('ticket').where({ id_usuario_aluno: id });
       if (result.length > 0) {
         return result[0];
       } else {
