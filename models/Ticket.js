8b8aa44b8abd4ee185183949a12fdc8782841124
andrezarzur <andrezarzur@hotmail.com>
2022-05-20 09:41:43 -0300

    ajuste

--- models/Ticket.js
+++ models/Ticket.js
@@ -74,6 +74,7 @@ class Ticket {
       var id = await knex.select(['id']).table('usuario').where({ sub: sub }).first();
       if (await knex.insert({id_usuario_aluno: id.id, corpo_texto: corpo_texto, data_criado: data_criado, data_fechado: null, data_limite: data_limite, feedback: null, id_processo_estagio: null, id_usuario_orientador: null}).table("ticket")){
         var id_ticket = await knex.select(['id']).table('ticket').where({feedback: null, id_usuario_aluno: id.id})
+        console.log(id_ticket)
         await knex.insert({ id_ticket: id_ticket.id, arquivo: doc1, tipo: "TCE", eProfessor: eProfessor}).table("documento");
         await knex.insert({ id_ticket: id_ticket.id, arquivo: doc2, tipo: "PA", eProfessor: eProfessor}).table("documento");
         return true;  
