952116d2bcd71f751665590cf8fb412bc9b7a4f5
andrezarzur <andrezarzur@hotmail.com>
2022-05-27 22:57:18 -0300

    ajustes

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,26 @@
-require("dotenv").config({ path: './.env' })
+require("dotenv").config()
+
+console.log(process.env.CLIENT)
+console.log(process.env.HOST)
+console.log(process.env.PORT)
+console.log(process.env.USER)
+console.log(process.env.PASSWORD)
+console.log(process.env.DATABASE)
 
 const knex = require('knex')({
-  client: process.env.CLIENT,
+  client: "postgresql",
   connection: {
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: "ec2-52-4-104-184.compute-1.amazonaws.com",
+    port: 5432,
+    user: "rojffajrdflnho",
+    password: "26d7426815121263da04cbd5b140ba5064dcff582cbed8da9aedaca8b6d11ab7",
+    database: "df0h2i2l2pfcdr",
     ssl: {
-      require: true, 
+      require: true,  
       rejectUnauthorized: false 
     }
   }
 });
+
 
 module.exports = knex
