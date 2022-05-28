7a0e554ca305cedcdc9b96afc0fbb34383486636
andrezarzur <andrezarzur@hotmail.com>
2022-05-28 00:21:22 -0300

    heroku

--- database/connection.js
+++ database/connection.js
@@ -1,20 +1,23 @@
-require('dotenv').config()
-console.log(process.env)
+require('dotenv').config();
+
+
+console.log(process.env.DB_HOST)
 const knex = require('knex')({
   debug: true,
   client: 'postgresql',
   connection: {
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USE,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USE,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     ssl: {
       require: true,  
       rejectUnauthorized: false 
     }
   }
-});
+})
+
 
 
 module.exports = knex
