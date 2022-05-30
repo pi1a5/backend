43adcc237f1c7031dc9196cfe7a4a6c385aa02c3
andrezarzur <andrezarzur@hotmail.com>
2022-05-30 18:01:14 -0300

    heroku

--- models/Ticket.js
+++ models/Ticket.js
@@ -198,7 +198,6 @@ class Ticket {
       var data_criado = new Date();
       var id = await knex.select(['id']).table('usuario').where({ sub: sub }).first();
       console.log(data_criado);
-      console.log(data);
       var id_existe = await knex.select(['id_processo_estagio']).table('ticket').where({id_usuario_aluno: id.id})
       if(id_existe.length == 0){ // se usuario não tem processo
         var id_processo_estagio = await knex.returning('id AS id_processo_estagio').insert({id_tipo_estagios: 0, situação: true, data_criado: data_criado, data_fechado: null}).table('processo_estagio')
