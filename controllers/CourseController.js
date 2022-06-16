4c034ade57886890c7bc76e3b6cb25d7d696e9e9
andrezarzur <andrezarzur@hotmail.com>
2022-06-16 18:58:19 -0300

    ajustes

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -5,7 +5,6 @@ class CouserController {
   async courses(req, res) {
     try {
       var course = await Course.findAll();
-      res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
       res.status(200).json(course);
     } catch (error) {
       res.status(500).json(error);
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 // const multer = require('multer')
 const router = require("./routes/routes")
 const app = express()
-const http = require("http").createServer(app)
+const http = require("http").createServer(app).setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
 const fileupload = require('express-fileupload')
 
 //const io = require('socket.io')(http)
