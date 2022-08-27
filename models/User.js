709a63dca83285b7b8247289f325bb63336a4d3c
andrezarzur <andrezarzur@hotmail.com>
2022-08-27 16:37:03 -0300

    ajuste

--- models/User.js
+++ models/User.js
@@ -29,7 +29,7 @@ class User {
       if (check.status !== 200) return { response: check.response, status: check.status };
 
       await knex.update({ idcurso: idCurso, prontuario: prontuario }).table('usuario').where({ sub });
-      return { response: 'Prontuário e curso atualizados', status: 200 };
+      return { response: user, status: 200 };
     } catch (error) {
       console.log(error);
       return { response: 'Erro ao atualizar base', status: 404 };
